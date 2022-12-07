8c225a93e8ab32cd0ce583d056ba76e8e565f8d1
Fadi <fady-naddaf@hotmail.com>
2022-12-08 00:00:22 +0100

    Creating category section

---
+++ src/components/Category.vue
@@ -0,0 +1,68 @@
+<template>
+  <main class="category-container">
+    <div class="category-title">
+      <h3>Categories</h3>
+      <span></span>
+    </div>
+    <div class="category-images">
+      <router-link to="/">
+        <img alt="banner-image" src="../assets/men-clothing.png" />
+      </router-link>
+      <router-link to="/">
+        <img alt="banner-image" src="../assets/women-clothing.png" />
+      </router-link>
+      <router-link to="/">
+        <img alt="banner-image" src="../assets/jewelery.png" />
+      </router-link>
+      <router-link to="/">
+        <img alt="banner-image" src="../assets/electronics.png" />
+      </router-link>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+};
+</script>
+
+<style scoped>
+.category-container {
+  display: flex;
+  flex-direction: column;
+  margin: 10% 0;
+  width: 100%;
+}
+.category-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 25px;
+}
+.category-title h3 {
+  font-size: 22px;
+  color: rgb(80, 80, 80);
+}
+.category-title span {
+  width: 50px;
+  height: 3px;
+  border-radius: 5px;
+  margin: -15px 0 15px 0;
+  background-color: #ff0084;
+}
+.category-images {
+  display: flex;
+  justify-content: space-evenly;
+}
+.category-images a {
+  width: 100%;
+}
+.category-images a:hover {
+  transform: translateY(-8px);
+}
+img {
+  width: 70%;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <NavBar />
   <HeroSection />
-  <!-- <category /> -->
+  <category />
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/Logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
@@ -12,14 +12,14 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue';
 import HeroSection from '@/components/HeroSection.vue';
-// import Category from '@/components/Category.vue';
+import Category from '@/components/Category.vue';
 
 export default {
   name: 'Home',
   components: {
     NavBar,
     HeroSection,
-    // Category,
+    Category,
   },
 };
 </script>
