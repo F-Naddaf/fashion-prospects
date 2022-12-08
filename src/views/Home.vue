e68ad4fd5d4eab0471b1add0a8a8171875370928
Fadi Naddaf <59022901+F-Naddaf@users.noreply.github.com>
2022-12-08 17:22:19 +0100

    Starting with the featured product (#5)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <NavBar />
   <HeroSection />
   <category />
+  <FeaturedProducts />
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/Logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
@@ -13,6 +14,7 @@
 import NavBar from '@/components/NavBar.vue';
 import HeroSection from '@/components/HeroSection.vue';
 import Category from '@/components/Category.vue';
+import FeaturedProducts from '@/components/FeaturedProducts.vue';
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -21,6 +23,7 @@ export default {
     NavBar,
     HeroSection,
     Category,
-  },
+    FeaturedProducts
+},
 };
 </script>
