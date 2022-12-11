a6b4f479a2f3bc09e2c71cc7bf7588dbfcdc35b0
Fadi <fady-naddaf@hotmail.com>
2022-12-11 14:40:30 +0100

    Creating a rating component

---
+++ src/components/ProductRate.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product-rate">
+    <span v-for="star in stars" :key="star[0]">
+      <i :class="`${star < rate ? ' ' : 'fa-dark'} fa-star fa-solid`"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductRate',
+  props: ['rate'],
+  data() {
+    return {
+      stars: [0, 1, 2, 3, 4],
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* .product-rate {
+  margin-top: -15px;
+} */
+i {
+  font-size: 12px;
+  padding-bottom: 15px;
+}
+.fa-star {
+  color: gold;
+}
+.fa-dark {
+  color: darkgray;
+}
+</style>
