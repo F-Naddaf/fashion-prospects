d33612c04ea54c481a57766f01368161fda311e8
Fadi <fady-naddaf@hotmail.com>
2022-12-14 02:13:25 +0100

    Creating carousel

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+import categoryRouter from "./routes/category.js";
+import shoppingCardRouter from "./routes/shoppingCart.js";
+import ordersRouter from "./routes/orders.js";
+import favoritesRouter from "./routes/favorites.js";
+
+// Create an express server
+const app = express();
+
+// Tell express to use the json middleware
+app.use(express.json());
+// Allow everyone to access our API. In a real application, we would need to restrict this!
+app.use(cors());
+
+/****** Attach routes ******/
+/**
+ * We use /api/ at the start of every route!
+ * As we also host our client code on heroku we want to separate the API endpoints.
+ */
+app.use("/api/categories", categoryRouter);
+app.use("/api/customer/shopping-cart", shoppingCardRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/user/favorite", favoritesRouter);
+export default app;
