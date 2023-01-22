81e1ab939c43fc58689bd4fcbbc726d43ad71ea7
Fadi <fady-naddaf@hotmail.com>
2023-01-22 15:44:18 +0100

    Fixing the main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,5 @@
-// import { createApp } from 'vue';
-// import App from './App.vue';
-// import router from './router';
-
-// createApp(App).use(router).mount('#app');
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import mitt from 'mitt';
 
-const event = new mitt();
-const app = createApp(App);
-app.config.globalProperties.$event = event;
-app.use(router).mount('#app');
-
+createApp(App).use(router).mount('#app');
